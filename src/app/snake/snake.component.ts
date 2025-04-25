eaf711c346dedc45ec7be2384a251466d058008e
JoaoVitorrFelix <joaovitor278891@gmail.com>
2025-04-24 21:26:40 -0300

    commit

---
+++ src/app/snake/snake.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-snake',
+  imports: [],
+  templateUrl: './snake.component.html',
+  styleUrl: './snake.component.css'
+})
+export class SnakeComponent {
+
+}
